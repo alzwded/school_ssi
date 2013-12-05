2688804da3f1b10929a1793b952e7dbc99d6f574
Vlad <alzwded@gmail.com>
2013-12-06 00:16:38 +0200

    update Makefile, memset arrays in main.c

--- Makefile
+++ Makefile
@@ -1,15 +1,19 @@
 CC = gcc
 F77 = gfortran
 #CFLAGS = -c -O3 -std=c99
-#F77FLAGS = -c -O3
-CFLAGS = -c -g -std=c99
-F77FLAGS = -c -g -cpp
+#F77FLAGS = -c -O3 -cpp
+CFLAGS = -c -g -std=c99 -I.
+F77FLAGS = -c -g -cpp -I. -std=gnu
 LD = gcc
 LDOPTS = -lm
 
+# C obj's the fortran obj's depend on
 COBJS = intlog.o
+# main fortran backend obj's
 FOBJS = enc.o tob64.o
+# main user-facing obj's
 OBJS = main.o
+# ...and dependencies are accounted for!
 
 ssi: $(COBJS) $(FOBJS) $(OBJS)
 	$(LD) -o ssi $(OBJS) $(FOBJS) $(COBJS) $(LDOPTS)
--- main.c
+++ main.c
@@ -6,16 +6,19 @@
 int main(int argc, char* argv[])
 {
     char* test1 = (char*)malloc(sizeof(char) * 100);
+    memset(test1, '\0', sizeof(char) * 100);
     strcpy(test1, "asd");
     int testLen = 100 * sizeof(char);
     char* testKey = "asdqwe";
     int testKeyLen = (int)strlen(testKey);
     char* test2 = (char*)malloc(sizeof(char) * 100);
+    memset(test2, '\0', sizeof(char) * 100);
     strcpy(test2, "no no no");
 
     enc_(test1, &testLen, testKey, &testKeyLen, test2);
 
     char* rrr = (char*)malloc(sizeof(char) * 201);
+    memset(rrr, '\0', sizeof(char) * 200);
     tob64_(test2, &testLen, rrr);
     rrr[200] = '\0';
 
